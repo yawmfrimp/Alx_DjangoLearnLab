c36daedb8ccf49a529e2d33af05ff5b62b0b3ded
Yaw Mfum-Frimpong <yawmfrimp@yahoo.com>
2025-07-23 15:43:38 +0000

    views modified

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,7 +7,7 @@ def list_books(request):
     """A function for viewing all books stored in the database."""
     books = Book.objects.all()
     context = {'book_list': books}
-    return render(request, 'books/book_list.html', context)
+    return render(request, 'relationship_app/list_books.html', context)
 
 class LibraryDetailView(DetailView):
     """ A class based view listing all details available for a specific library """
