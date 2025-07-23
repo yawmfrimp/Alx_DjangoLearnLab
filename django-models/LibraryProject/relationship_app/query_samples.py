2f7460552f95f6a117de874bc0a79d5e2e54b135
Yaw Mfum-Frimpong <yawmfrimp@yahoo.com>
2025-07-23 14:12:41 +0000

    changes to queries

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -4,5 +4,6 @@ from models import Book,Library,Librarian
 book = Book.objects.filter(author=author_name)
 
 books = Library.objects.get(name=library_name)
+books = Library.books.all()
 
 librarian = Librarian.objects.get(library=library_name)
