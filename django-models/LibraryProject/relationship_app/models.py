c51b2f6bd041b84563db2228d7caf812a2bef4d2
Yaw Mfum-Frimpong <yawmfrimp@yahoo.com>
2025-07-23 14:06:49 +0000

    added str function to models

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -4,15 +4,26 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
 
+    def __str__(self):
+        return f'{self.title} by {self.author}'
+    
 class Library(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book, related_name='libraries')
 
+    def  __str__(self):
+        return self.name
+    
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f'{self.name} of {self.library}'
