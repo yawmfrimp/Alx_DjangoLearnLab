25733bc5615255c82fcc61f70b92b1fc78599d0a
Yaw Mfum-Frimpong <yawmfrimp@yahoo.com>
2025-07-23 23:08:55 +0000

    modified views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -20,7 +20,7 @@ class LibraryDetailView(DetailView):
     template_name = 'relationship_app/library_detail.html'
 
 class RegisterView(CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationForm()
     success_url = reverse_lazy('login')
     template_name = 'relationship_app/register.html'
 
