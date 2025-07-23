d6df7f56d58b32d160507aea2c23a718fa589280
Yaw Mfum-Frimpong <yawmfrimp@yahoo.com>
2025-07-23 23:03:49 +0000

    modified views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import DetailView, CreateView
+from django.contrib.auth import login,logout
 from django.contrib.auth.forms import UserCreationForm
 from .models import Library, Book
 
