36bf0f2df1e00bc17ba4435fe232c3036487c454
Yaw Mfum-Frimpong <yawmfrimp@yahoo.com>
2025-07-24 15:14:24 +0000

    cosmetic edits to paths

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -29,8 +29,8 @@ urlpatterns = [
     path('admin-view/', views.admin_view, name='admin_view'),
     path('member-view/', views.member_view, name='member_view'),
     path('librarian-view/', views.librarian_view, name='librarian_view'),
-    path('books/add/', views.add_book, name='add_book'),
-    path('books/<int:pk>/edit/', views.edit_book, name='edit_book'),
-    path('books/<int:pk>/delete', views.delete_book, name='delete_book')
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),
+    path('delete_book/<int:pk/', views.delete_book, name='delete_book')
 
 ]
