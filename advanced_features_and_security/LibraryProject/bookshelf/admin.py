3f48938f436c4cd642156f2d6c857b6e83ed55d5
Yaw Mfum-Frimpong <yawmfrimp@yahoo.com>
2025-08-04 00:23:27 +0000

    added permissions and groups

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Book
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
+from django.contrib.auth.models import Group , Permission
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
@@ -15,9 +16,30 @@ class CustomUserAdmin(UserAdmin):
     add_fieldsets = UserAdmin.add_fieldsets + (
         ('Extra Fields', {'fields': ('date_of_birth',)})
     )
-
-
 # Register your models here.
 admin.site.register(Book, BookAdmin)
 
 admin.site.register(CustomUser, CustomUserAdmin)
+
+# creating or fetching the group
+'''The following groups have been set up: editors, viewers, admins'''
+Editors, created = Group.objects.get_or_create(name='Editors')
+Viewers, created = Group.objects.get_or_create(name='Viewers')
+Admins, created = Group.objects.get_or_create(name='Admins')
+
+#fetch the permissions you want
+'''The permissions created in views have been fetched into the variables
+can_view_perm for the can_view permission, can_create_perm for the can_create permission
+can_edit_perm for the can_edit_permission and the can_delete_perm for the can_delete_permission '''
+can_view_perm  = Permission.objects.get(codename='can_view')
+can_create_perm = Permission.objects.get(codename='can_create')
+can_edit_perm = Permission.objects.get(codename='can_edit')
+can_delete_perm = Permission.objects.get(codename='can_delete')
+
+#assigning them to the group
+'''The permissions fetched above are then assigned to the various groups created
+editors get the can_view, can_create and can_edit permission, viewers get only the can_view permission
+and the admins get all the permissions'''
+Editors.permissions.add(can_create_perm,can_view_perm,can_edit_perm)
+Viewers.permissions.add(can_view_perm)
+Admins.permissions.add(can_create_perm,can_delete_perm,can_edit_perm,can_view_perm)
---
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
+from django.contrib.auth.decorators import permission_required
+from django.forms import modelform_factory
+from django.contrib import messages
+from .models import Book
+
+# Create your views here.
+def detail(request, book_id):
+    return HttpResponse('You\'re looking at book %s.' % book_id)
+
+BookForm = modelform_factory(Book, fields=['title', 'author', 'publication_year'])
+
+def list_books(request):
+    '''This view is to list all available books in the database'''
+    if request.method == 'POST':
+        books = Book.objects.all()
+        context = {'book_list', books}
+        return render(request, 'bookshelf/list_books.html', context)
+    
+@permission_required('bookshelf.can_create', raise_exception=True)
+def create_model(request):
+    '''This view is to create a new book, only users with the 'can_create' permission can access this view'''
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('list_books')
+        else:
+            form = BookForm()
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_model(request,pk):
+    '''This view is to edit an existing book, only users with the 'can_edit' permission can access this view'''
+    if request.method == 'POST':
+        book = get_object_or_404(Book, pk=pk)
+        if request.method == 'POST':
+            form = BookForm(request.POST, instance=book)
+            if form.is_valid():
+                form.save()
+                return redirect('list_books')
+    else:
+        form = BookForm(instance=book)
+        return render(request, 'bookshelf/book_form.html', {
+        'form' : form,
+        'edit' : True,
+        'book' : book,
+    })
+
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_model(request,pk):
+    '''This view is to delete an existing book, only users with the 'can_delete' permission can access this view'''    
+    if request.method == 'POST':
+        book = get_object_or_404(Book, pk=pk)
+        book.delete()
+        messages.success(request,f'"{book.title}" was deleted.')
+        return redirect('list_books')
+    return render(request, 'bookshelf/book_confirm_delete.html', {'book': book})    
+
+
